07157a5cf9c5cd0d66d331673a6a142016314114
Leandro Biazão <leandro.biazao@csgi.com>
2024-01-19 19:14:47 -0500

    Improve by remove unacessary lines

--- hello.py
+++ hello.py
@@ -1,11 +1,8 @@
 # Ask user for their name
 name = input("What's your name? ")
 
-# Remover whitespace from str
-name = name.strip()
-
-#Capitalize user's name
-name = name.title()
+# Remover whitespace from str and capitalize user's name 
+name = name.strip().title()
 
 # Say hello to user
 print(f"Hello, {name}")
