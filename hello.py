9317b780b6f01a4250421e9ae6fa6fb99013123d
Leandro Biazão <leandro.biazao@csgi.com>
2024-01-20 19:28:19 -0500

    How to split an string.

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 # Ask user for their name, remove spaces and capitalize the name input
 name = input("What's your name? ").strip().title()
 
+# Split full name into first and last name.
+firstname, lastname = name.split(' ')
+
 # Say hello to user
-print(f"Hello, {name}")
+print(f"Hello, {firstname}")
